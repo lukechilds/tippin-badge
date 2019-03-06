d8deaf90fbd580e684900082383ace7c1390de93
Luke Childs <lukechilds123@gmail.com>
2019-03-06 08:44:21 +0700

    Generate SVG preview client side

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
+const badgen = require('badgen');
 const escape = require('escape-html');
 
 const badgeLinkUrl = username => `https://tippin.me/@${username}`;
 const badgeImageUrl = username => `https://badgen.net/badge/%E2%9A%A1%EF%B8%8Ftippin.me/@${username}/F0918E`;
+
+const badgeSVG = username => badgen({
+  subject: '⚡️tippin.me',
+  status: `@${username}`,
+  color: 'F0918E',
+});
+
+const badgeHtmlPreview = username =>
+`<a href="${badgeLinkUrl(username)}" rel="noopener noreferrer" target="_blank">
+  ${badgeSVG(username)}
+</a>`;
 
 const badgeHtml = username =>
 `<a href="${badgeLinkUrl(username)}" rel="noopener noreferrer" target="_blank">
@@ -15,15 +27,16 @@ const usernameInput = document.querySelector('input[name="username"]');
 const badgePreview = document.querySelector('.badge-preview');
 const badgeHtmlSource = document.querySelector('.badge-html');
 const badgeMarkdownSource = document.querySelector('.badge-markdown');
+const badgeSvgSource = document.querySelector('.badge-svg');
 
 const generateBadge = () => {
   const username = escape(usernameInput.value) || 'username';
-  const html = badgeHtml(username);
-  const markdown = badgeMarkdown(username);
 
-  badgePreview.innerHTML = html;
-  badgeHtmlSource.innerText = html;
-  badgeMarkdownSource.innerText = markdown;
+  badgePreview.innerHTML = badgeHtmlPreview(username);
+
+  badgeHtmlSource.innerText = badgeHtml(username);
+  badgeMarkdownSource.innerText = badgeMarkdown(username);
+  badgeSvgSource.innerText = badgeSVG(username);
 };
 
 generateBadge();
